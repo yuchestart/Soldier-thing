980eb81e31d02d4d3fb23ec0854a80ab770097de
What-ever-this-is <yuchestart@gmail.com>
2023-04-01 21:27:07 -0500

    Add references to enemy sprites

--- resources/js/vars.js
+++ resources/js/vars.js
@@ -69,6 +69,32 @@ const characterFrames ={
     aimfire:image("./resources/images/character/SoldierAimFire.svg"),
     fire:image("./resources/images/character/SoldierFire.svg")
 }
+const enemyFrames = {
+    idle:image("./resources/images/enemy/SoldierIdle.svg"),
+    walk1:image("./resources/images/enemy/SoldierWalk1.svg"),
+    walk2:image("./resources/images/enemy/SoldierWalk2.svg"),
+    walk3:image("./resources/images/enemy/SoldierWalk3.svg"),
+    walk4:image("./resources/images/enemy/SoldierWalk4.svg"),
+    walk5:image("./resources/images/enemy/SoldierWalk5.svg"),
+    walk6:image("./resources/images/enemy/SoldierWalk6.svg"),
+    reload1:image("./resources/images/enemy/SoldierReload1.svg"),
+    reload2:image("./resources/images/enemy/SoldierReload2.svg"),
+    reload3:image("./resources/images/enemy/SoldierReload3.svg"),
+    reload4:image("./resources/images/enemy/SoldierReload4.svg"),
+    reload5:image("./resources/images/enemy/SoldierReload5.svg"),
+    reload6:image("./resources/images/enemy/SoldierReload6.svg"),
+    reload7:image("./resources/images/enemy/SoldierReload7.svg"),
+    reload8:image("./resources/images/enemy/SoldierReload8.svg"),
+    reload9:image("./resources/images/enemy/SoldierReload9.svg"),
+    reload10:image("./resources/images/enemy/SoldierReload10.svg"),
+    reload11:image("./resources/images/enemy/SoldierReload11.svg"),
+    reload12:image("./resources/images/enemy/SoldierReload12.svg"),
+    reload13:image("./resources/images/enemy/SoldierReload13.svg"),
+    reload14:image("./resources/images/enemy/SoldierReload14.svg"),
+    aim:image("./resources/images/enemy/SoldierAim.svg"),
+    aimfire:image("./resources/images/enemy/SoldierAimFire.svg"),
+    fire:image("./resources/images/enemy/SoldierFire.svg")
+}
 const player = new Soldier(10,34)
 const enemies = [new Soldier(10,34,"enemy",1000,0.4,700,500),new Soldier(10,34,"enemy",1000,0.4,700,400),new Soldier(10,34,"enemy",1000,0.4,700,300)]
 const mission = {
